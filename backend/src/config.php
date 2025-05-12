801cbdd610b1031a4bc68ef3b888d4f5d50323d0
Alex <79389493+alexelkinx@users.noreply.github.com>
2025-05-12 03:40:16 +0300

    Update config.php

--- backend/src/config.php
+++ backend/src/config.php
@@ -4,5 +4,5 @@ define('ROOT', dirname(__DIR__));
 
 define('DB_HOST', 'localhost:3306');
 define('DB_NAME', 'citybike');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_USER', 'admin');
+define('DB_PASS', 'changeme');
